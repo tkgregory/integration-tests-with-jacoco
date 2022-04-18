3b84ce19d0a08bf748c2049b683abaa2bb8a536f
Tom Gregory <t.k.gregory@gmail.com>
2022-04-18 17:26:48 +0200

    Get unit tests to 100%.

--- src/test/java/com/tom/service/MathServiceTest.java
+++ src/test/java/com/tom/service/MathServiceTest.java
@@ -20,4 +20,11 @@ public class MathServiceTest {
 
         assertThat(result, equalTo(8));
     }
+
+    @Test
+    public void subtractNumbers() {
+        int result = mathService.subtract(5, 3);
+
+        assertThat(result, equalTo(2));
+    }
 }
